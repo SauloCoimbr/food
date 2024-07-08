56d03f03af0cfaec2ddb0a89d06ad255327239b6
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2024-07-07 22:50:14 -0300

    Text Project Food

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-gray w-full h-full">
+      <Text>Teste 123</Text>
+    </View>
+  );
+}
